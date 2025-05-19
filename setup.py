c7a66db9e019e1f3771c0ffb622afae9fed1f4b9
Rishabh Goel <rishabhgoel0213@gmail.com>
2025-05-19 10:44:52 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,4 +3,6 @@ from setuptools import setup, find_packages
 setup(
   name="game-engine",
   version="0.1.0",
+  packages=find_packages(include=["engine=", "model="]),
+  exclude_package_data("": ["configs/*", "server/*", "scripts/*", "tests/*"])
 )
